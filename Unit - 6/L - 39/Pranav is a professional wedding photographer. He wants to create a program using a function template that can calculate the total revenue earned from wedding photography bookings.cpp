c0a83392a6be9af425eefe2bee3b86d77b62f1de
Hukam <115365309+rathorehukam@users.noreply.github.com>
2023-11-09 21:51:06 +0530

    Create Pranav is a professional wedding photographer. He wants to create a program using a function template that can calculate the total revenue earned from wedding photography bookings.cpp

---
+++ Unit - 6/L - 39/Pranav is a professional wedding photographer. He wants to create a program using a function template that can calculate the total revenue earned from wedding photography bookings.cpp
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+
+
+#include <iostream>
+#include <vector>
+
+template <typename T>
+T calculateTotalRevenue(const std::vector<T>& bookings) {
+    T totalRevenue = 0;
+    for (const T& amount : bookings) {
+        totalRevenue += amount;
+    }
+    return totalRevenue;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::vector<double> bookingAmounts(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> bookingAmounts[i];
+    }
+
+    double totalRevenue = calculateTotalRevenue(bookingAmounts);
+
+    std::cout << totalRevenue << std::endl;
+
+    return 0;
+}
