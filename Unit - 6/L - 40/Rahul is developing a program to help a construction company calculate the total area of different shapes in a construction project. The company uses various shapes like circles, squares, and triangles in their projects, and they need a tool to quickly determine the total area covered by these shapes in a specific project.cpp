1134dc8d8adc6e827db8d7a66ebb5bc149e98c50
Hukam <115365309+rathorehukam@users.noreply.github.com>
2023-11-11 18:14:06 +0530

    Update Rahul is developing a program to help a construction company calculate the total area of different shapes in a construction project. The company uses various shapes like circles, squares, and triangles in their projects, and they need a tool to quickly determine the total area covered by these shapes in a specific project.cpp

--- Unit - 6/L - 40/Rahul is developing a program to help a construction company calculate the total area of different shapes in a construction project. The company uses various shapes like circles, squares, and triangles in their projects, and they need a tool to quickly determine the total area covered by these shapes in a specific project.cpp
+++ Unit - 6/L - 40/Rahul is developing a program to help a construction company calculate the total area of different shapes in a construction project. The company uses various shapes like circles, squares, and triangles in their projects, and they need a tool to quickly determine the total area covered by these shapes in a specific project.cpp
@@ -10,12 +10,51 @@
 
 
 
+
 #include <iostream>
 #include <iomanip>
 #include <cmath>
 #include <limits>
 
-// ... (previous code)
+// Define class templates for Circle, Square, and Triangle
+template <typename T>
+class Circle {
+private:
+    T radius;
+
+public:
+    Circle(T r) : radius(r) {}
+
+    T area() const {
+        return M_PI * radius * radius;
+    }
+};
+
+template <typename T>
+class Square {
+private:
+    T side;
+
+public:
+    Square(T s) : side(s) {}
+
+    T area() const {
+        return side * side;
+    }
+};
+
+template <typename T>
+class Triangle {
+private:
+    T base, height;
+
+public:
+    Triangle(T b, T h) : base(b), height(h) {}
+
+    T area() const {
+        return 0.5 * base * height;
+    }
+};
 
 int main() {
     std::string circleName, squareName, triangleName;
@@ -25,11 +64,9 @@ int main() {
     std::getline(std::cin, circleName);
     std::cin >> circleRadius;
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // clear buffer
-
     std::getline(std::cin, squareName);
     std::cin >> squareSide;
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // clear buffer
-
     std::getline(std::cin, triangleName);
     std::cin >> triangleBase >> triangleHeight;
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // clear buffer
@@ -39,9 +76,10 @@ int main() {
     Square<double> square(squareSide);
     Triangle<double> triangle(triangleBase, triangleHeight);
 
-    double totalArea = circle.calculateArea() + square.calculateArea() + triangle.calculateArea();
+    // Calculate total area
+    double totalArea = circle.area() + square.area() + triangle.area();
 
-    // Output with fixed precision
+    // Output
     std::cout << std::fixed << std::setprecision(2);
     std::cout << "Total area of all shapes: " << totalArea << std::endl;
 
