4b545dd75aca85014fa91aa9b80b6aea900e26ef
Hukam <115365309+rathorehukam@users.noreply.github.com>
2023-11-12 10:11:53 +0530

    Update cy In $$ a data analysis system, you are provided with an array of integer elements representing the scores of students in a game. Your task is to process the scores and convert the array into its reduced form using vectors of pairs. Additionally, you need to implement two functions, convert() and printArr() to handle the conversion and display the reduced form, respectively.cpp

--- Unit - 6/L - 40/cy In $$ a data analysis system, you are provided with an array of integer elements representing the scores of students in a game. Your task is to process the scores and convert the array into its reduced form using vectors of pairs. Additionally, you need to implement two functions, convert() and printArr() to handle the conversion and display the reduced form, respectively.cpp
+++ Unit - 6/L - 40/cy In $$ a data analysis system, you are provided with an array of integer elements representing the scores of students in a game. Your task is to process the scores and convert the array into its reduced form using vectors of pairs. Additionally, you need to implement two functions, convert() and printArr() to handle the conversion and display the reduced form, respectively.cpp
@@ -1,1 +1,36 @@
 
+
+
+
+// You are using GCC
+#include <bits/stdc++.h> 
+using namespace std; 
+void convert(int arr[], int n) {
+    vector <pair<int, int> > v; 
+    for (int i = 0; i < n; i++) 
+        v.push_back(make_pair(arr[i], i)); 
+    sort(v.begin(), v.end()); 
+    for (int i=0; i<n; i++) 
+        arr[v[i].second] = i; 
+} 
+void printArr(int arr[], int n) { 
+    for (int i=0; i<n; i++) 
+        cout << arr[i] << " "; 
+} 
+int main(){ 
+    int n; 
+    cin>>n;
+    if(n<1||n>15) {
+        cout<<"-1";
+        return 0;
+    }
+    int arr[n];
+    for(int i =0;i<n;i++)
+        cin>>arr[i];
+    cout<<"Input Array: ";
+    printArr(arr, n); 
+    convert(arr , n); 
+    cout << "\nConverted Array: "; 
+    printArr(arr, n); 
+    return 0; 
+} 
