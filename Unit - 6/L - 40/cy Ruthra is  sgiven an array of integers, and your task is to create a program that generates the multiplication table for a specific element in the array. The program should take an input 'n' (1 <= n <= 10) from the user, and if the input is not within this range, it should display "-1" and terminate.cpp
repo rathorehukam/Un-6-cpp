42f489204658ab9440b89aaa4e8fb384ef984e31
Hukam <115365309+rathorehukam@users.noreply.github.com>
2023-11-12 08:38:03 +0530

    Update cy Ruthra is  sgiven an array of integers, and your task is to create a program that generates the multiplication table for a specific element in the array. The program should take an input 'n' (1 <= n <= 10) from the user, and if the input is not within this range, it should display "-1" and terminate.cpp

--- Unit - 6/L - 40/cy Ruthra is  sgiven an array of integers, and your task is to create a program that generates the multiplication table for a specific element in the array. The program should take an input 'n' (1 <= n <= 10) from the user, and if the input is not within this range, it should display "-1" and terminate.cpp
+++ Unit - 6/L - 40/cy Ruthra is  sgiven an array of integers, and your task is to create a program that generates the multiplication table for a specific element in the array. The program should take an input 'n' (1 <= n <= 10) from the user, and if the input is not within this range, it should display "-1" and terminate.cpp
@@ -4,14 +4,9 @@
 
 
 
-
-
-
-
-
-
 #include <iostream>
 #include <vector>
+#include <iterator>
 
 int main() {
     std::vector<int> arr = {11, 21, 33, 14, 41, 60, 13, 8, 25, 50};
@@ -21,16 +16,16 @@ int main() {
 
     if (n < 1 || n > 10) {
         std::cout << "-1" << std::endl;
-        return -1; // Terminate with an error code
+        return 1; // Terminating with an error code
     }
 
-    auto it = arr.begin() + n - 1;
-    // Get the n-th element from the array
+    std::vector<int>::iterator it = arr.begin();
+    std::advance(it, n - 1); // Adjust for 0-based indexing
+
     int baseNumber = *it;
-
     for (int i = 1; i <= 10; ++i) {
-        std::cout << baseNumber << " * " << i << " = " << (baseNumber * i) << std::endl;
+        std::cout << baseNumber << " * " << i << " = " << baseNumber * i << std::endl;
     }
 
-    return 0; // Successful execution
+    return 0; // Program executed successfully
 }
